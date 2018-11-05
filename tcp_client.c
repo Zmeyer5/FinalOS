c55031bdddd02d5ced27920b4f97b4b37d1ae9ac
Zmeyer5 <meyerzachary5@gmail.com>
2018-11-05 15:56:29 -0500

    created shared memory
    Had to change what is returned by void *run_client() because I was still getting errors. Client reads in a string from a .txt file and places it in shared memory. Server reads shared memory and sends shared memory message back to client.

--- tcp_client.c
+++ tcp_client.c
@@ -1,97 +1,148 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<time.h>
 #include<string.h>
 #include<arpa/inet.h>
 #include <errno.h>
+#include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
-#define MAX  1000
 
-
+#define MAX  15
+#define PORT_NUM 7891
 
 
 void delay();
-void run_client();
+void *run_client(void * in_arg);
+void create_threads(const char * ip_addr);
+void delete_thread(pthread_t threadArray[3]);
 
-
-
-int main() {
-	run_client();
-	return 0;
+int main(int argc, char **argv) {
+    if(argc != 2) {
+        printf("Usage: /{filename} {server_ip}\n");
+        return -1;
+    }
+    char * ip_addr = argv[1];
+    
+    create_threads(ip_addr);
+    return 0;
 }
 
+
+
+void create_threads(const char * ip_addr){
+    //Initializing threads
+    pthread_t threadArray[3];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    
+    //Creating three writer threads
+    int i;
+    for(i = 0; i < 3; i++){
+        if (pthread_create(&threadArray[i], NULL, run_client, (void *)ip_addr) != 0) {
+            perror("Unable to create thread to handle new connection");
+            exit(EXIT_FAILURE);
+        }
+    }
+    delete_thread(threadArray);
+}
+
+void delete_thread(pthread_t threadArray[3]){
+    int i;
+    for(i = 0; i < 3; i++){
+        pthread_join(threadArray[i],NULL);
+    }
+}
 
 
 void delay(int number_of_seconds){
-	// Converting time into milli_seconds
-	int milli_seconds = 1000 * number_of_seconds;
-
-	// Stroing start time
-	clock_t start_time = clock();
-
-	// looping till required time is not acheived
-	while (clock() < start_time + milli_seconds);
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * number_of_seconds;
+    
+    // Stroing start time
+    clock_t start_time = clock();
+    
+    // looping till required time is not acheived
+    while (clock() < start_time + milli_seconds);
 }
 
 
 
-void run_client(){
-	char recieved[MAX], to_send[MAX], username[MAX];
-	int net_socket, conn_stat, valread;	
-	struct sockaddr_in server_address;
-	socklen_t  address_size;
-
-	if((net_socket = socket(PF_INET, SOCK_STREAM, 0)) == -1){
-		perror("Socket Failed");
-		exit(EXIT_FAILURE);
-	}
-
-	//Server address is IPv4 and port is 9005 - Initialize server_address to NULL	
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(7891);	
-	server_address.sin_addr.s_addr = inet_addr("127.0.0.1");	
-
-	memset(server_address.sin_zero, '\0', sizeof server_address.sin_zero);
-
-	address_size = sizeof server_address;
-	if(connect(net_socket, (struct sockaddr *) &server_address, address_size) == -1){
-		printf("There was an error connecting to the socket\n");
-		printf("ERRNO %s",strerror(errno));
-		exit(EXIT_FAILURE);
-
-	}
-
-
-	while (1) {
-		recv(net_socket, recieved, sizeof(recieved),0);
-		
-		printf("Server: ");
-		printf("%s\n", recieved);
-		if (strcmp(recieved, "exit") == 0) {
-			delay(3);
-			return;
-		}
-		memset(recieved, 0, sizeof(recieved));
-
-		//Sending message to server
-		printf("%s: ", username);
-		memset(to_send, 0, sizeof(to_send));
-		
-		fgets(to_send, MAX, stdin);
-		printf("\n");
-		
-		//Sending message to server
-		if (strcmp(to_send, "exit\n") == 0) {
-			printf("Exiting...\n");
-			send(net_socket, "exit", sizeof(username), 0);
-			exit(0);
-		}
-		send(net_socket, to_send, strlen(to_send), 0);
-	}
-
-	return;
-
-
+void *run_client(void * in_arg){
+    const char * ip_addr  = (const char *)in_arg;
+    char received[MAX], to_send[MAX], username[MAX];
+    int net_socket, conn_stat, valread;
+    struct sockaddr_in server_address;
+    socklen_t address_size;
+    
+    //FILE * fp = fopen("mydata.txt", "r");
+    
+    //creates shared memory
+    key_t key = ftok("shmfile",65);
+    int shmid = shmget(key,1024,0666|IPC_CREAT);
+    char *str = (char*) shmat(shmid,(void*)0,0);
+    int i = 0;
+    
+    //reads in message of max length 15 from a txt file
+    FILE *ptrFile;
+    char word[15];
+    char *filename = "message.txt";
+    ptrFile = fopen(filename,"r");
+    if(ptrFile == NULL){
+        return 1;
+    }
+    fgets(word,15,ptrFile);
+    
+    
+    //places string from file in shared memory
+    while (word[i]!='\0')
+    {
+        str[i] = word[i];
+        i++;
+    }
+    printf("Shared memory created.\n");
+    fclose(ptrFile);
+    shmdt(str);
+    
+    if((net_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("Socket Failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    //Server address is IPv4 and port is PORT_NUM
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT_NUM);
+    server_address.sin_addr.s_addr = inet_addr(ip_addr);
+    
+    // address_size = sizeof server_address;
+    if(connect(net_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1){
+        printf("There was an error connecting to the socket\n");
+        printf("ERRNO %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    
+    // TODO - Shared Buffer needs to be created
+    memset(to_send, 0, sizeof(to_send));
+    // fgets(shared_buffer, MAX, fp);
+    
+    
+    //Sending message to server
+    if(send(net_socket, to_send, strlen(to_send), 0) < 0) {
+        printf("Error: send() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Receive reply and terminate
+    if(recv(net_socket, received, sizeof(received), 0) < 0) {
+        printf("Error: recv() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("Server: %s\n", received);
+    
+    return 1;
 }
--- tcp_server.c
+++ tcp_server.c
@@ -13,120 +13,118 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 
 
 #define MAX 1000
-
+#define PORT_NUM 7891
 
 
 void create_server();
-void messager(int new_socket);
+void *messager(void *in_arg);
 
 
 
 int main(){
-	create_server();
-	return 0;
+    create_server();
+    return 0;
 }
 
 
 
 void create_server(){
-
+    
     int server_fd, new_socket;
    	struct sockaddr_storage server_storage;
-	socklen_t addr_size;
-	struct sockaddr_in address;
+    socklen_t addr_size;
+    struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-   
-	
-	// Creating socket file descriptor
-	//Socket is using IPv4 and TCP. Checks if NULL
+    pthread_t thread_id;
+    
+    
+    // Creating socket file descriptor
+    //Socket is using IPv4 and TCP. Checks if NULL
     if ((server_fd = socket(PF_INET, SOCK_STREAM, 0)) == -1){
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-
-	//Address is a struct of sockaddr_in and uses IPv4 and the localhost for IP
-    address.sin_family = AF_INET; 
-    address.sin_port = htons(7891);
-	address.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-
-    // Forcefully attaching socket to the port 8080
+    
+    //Address is a struct of sockaddr_in and uses IPv4 and the localhost for IP
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT_NUM);
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    
+    // Forcefully attaching socket to the port PORT_NUM
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0){
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-	printf("Listening...\n");
+    printf("Listening...\n");
     if (listen(server_fd, 3) < 0){
         perror("listen");
         exit(EXIT_FAILURE);
     }
-	addr_size = sizeof server_storage;
-	if ((new_socket = accept(server_fd, (struct sockaddr *) &server_storage, &addr_size)) < 0){
-			perror("accept");
-			exit(EXIT_FAILURE);
-	}
-
-
-	messager(new_socket);
+    
+    int remaining_connections = 3; // TODO: change while condition to check shared memory for notification to terminate?
+    while (remaining_connections > 0)  {
+        addr_size = sizeof(server_storage);
+        if ((new_socket = accept(server_fd, (struct sockaddr *) &server_storage, &addr_size)) < 0){
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        if (pthread_create(&thread_id, NULL, messager, (void *)(intptr_t)new_socket) != 0) {
+            perror("Unable to create thread to handle new connection");
+            exit(EXIT_FAILURE);
+        }
+        remaining_connections--;
+    }
 }
 
 
 
-void messager(int new_socket){
-    char buffer[MAX] = {0}, message[MAX];
-	char hello[] = 
-"\n   *****************************\n\
-   *                           *\n\
-   *       Welcome to the      *\n\
-   *           server!         *\n\
-   *                           *\n\
-   *                           *\n\
-   *****************************\n\n\n\n\n";
- 
-	
-	
-	
-	//Send server greeting message
-	send(new_socket, hello, strlen(hello),0);
-
-
-	//If user enters "exit" terminate
-	while(1){
-		
-		recv(new_socket, buffer, 1024,0);
-	
-		if(strcmp(buffer, "exit") == 0){
-			recv(new_socket, buffer, MAX, 0);
-			printf("Client has left the server\n");
-			return;
-		}	
-		
-		printf("Other user: %s", buffer);
-
-		//Input message from stdin
-		printf(">>");
-		fgets(message,MAX, stdin);
-		printf("\n");
-		
-		if(strcmp(message,"exit") == 0){
-			printf("Exiting...\n");
-			send(new_socket, "exit\n", sizeof(message),0);
-			return;
-		}
-
-		//Send and reset buffer
-		send(new_socket, message, strlen(message), 0);
-		memset(message, 0, sizeof message);
-		memset(buffer, 0, 1024);
-	}
-
-	close(new_socket); 
-    return;
-
-
+void *messager(void *in_arg){
+    int new_socket = (intptr_t) in_arg;
+    char buffer[MAX] = {0};
+    char *message = "testing, testing, 1, 2, 3..."; // Test response
+    char hello[] =
+    "\n   *****************************\n\
+    *                           *\n\
+    *       Welcome to the      *\n\
+    *           server!         *\n\
+    *                           *\n\
+    *                           *\n\
+    *****************************\n\n\n\n\n";
+    
+    //create shared memory str
+    key_t key = ftok("shmfile",65);
+    int shmid = shmget(key,1024,0666|IPC_CREAT);
+    char *str = (char*) shmat(shmid,(void*)0,0);
+    
+    // Send server greeting message
+    send(new_socket, hello, strlen(hello),0);
+    
+    // Receive writer message
+    recv(new_socket, buffer, 1024,0);
+    
+    //prints shared memory as long as its not empty
+    if (strlen(str) != 0)
+        printf("DATA READ FROM SHARED MEMORY: %s\n", str);
+    //printf("SENDING MESSAGE\n");
+    sleep(2);
+    
+    //sends shared memory contents with client
+    send(new_socket, str, strlen(str), 0);
+    
+    shmdt(str);
+    shmctl(shmid,IPC_RMID,NULL);
+    
+    //printf("Other user: %s", buffer);
+    
+    
+    close(new_socket);
+    pthread_exit(NULL);
 }
